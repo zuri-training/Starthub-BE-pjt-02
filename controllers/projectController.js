db1e64ddab705a85e13c2b7bb21f95ec4a7b5729
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-06-19 13:08:47 +0100

    project CRUD operations and User Sign Up

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -33,31 +33,47 @@ exports.getProjects = async (req, res) => {
 };
 
 exports.getProject = async (req, res) => {
-  const project = await Project.findById( req.params.id );
+  const project = await Project.findById(req.params.id);
 
   try {
-    res.status(200).json({
-      message: "successfully fetched the requested project",
-      data: project,
-    });
+    if (!project) {
+      res.status(404).json({
+        message: "project with that id not found",
+      });
+    } else {
+      res.status(200).json({
+        message: "successfully fetched the requested project",
+        data: project,
+      });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 exports.updateProject = async (req, res) => {
-  const updatedProject = await Project.findByIdAndUpdate(req.params.id, {
-    ...req.body
-    // projectName: req.body.projectName,
-    // projectInfo: req.body.projectInfo,
-    // projectImage: req.body.projectImage,
-    // projectUrl: req.body.projectUrl,
-  });
+  const updatedProject = await Project.findByIdAndUpdate(
+    req.params.id,
+    {
+      // ...req.body,
+      projectName: req.body.projectName,
+      projectInfo: req.body.projectInfo,
+      projectImage: req.body.projectImage,
+      projectUrl: req.body.projectUrl,
+    },
+    { new: true }
+  );
 
   try {
-    res
-      .status(200)
-      .json({ message: "project updated successfully", data: updatedProject });
+    if (!updatedProject) {
+      res.status(404).json({ message: "project with that id not found" });
+    } else {
+      updatedProject.save();
+      res.status(200).json({
+        message: "project updated successfully",
+        data: updatedProject,
+      });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -67,7 +83,11 @@ exports.deleteProject = async (req, res) => {
   const deletedProject = await Project.findByIdAndDelete(req.params.id);
 
   try {
-    res.status(200).json({ message: "project deleted successfully" });
+    if (!deletedProject) {
+      res.status(404).json({ message: "project with id not found" });
+    } else {
+      res.status(200).json({ message: "project deleted successfully" });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
