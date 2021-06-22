ca2de850a91f748cb69bfb649e09922f67974fd0
izaakwalz <iblack.xyz@gmail.com>
2021-06-22 04:06:25 +0100

    added error handler middleware and auth routes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
 //import db connection
-const connectDB = require("./db");
+const connectDB = require('./db');
+// import error middlewares
+const { notFound, errorHandler } = require('./middleware/errorHandler');
 
 // import routes
 const projectRoutes = require('./routes/projectRoutes');
@@ -21,10 +23,16 @@ connectDB();
 // routes
 app.use(projectRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/api/v1', require('./routes/index.routes'));
 
-const port = process.env.PORT || 3000
+app.get('/', (req, res) => {
+  res.send('API IS RUNNING 🚨 🚨 🚨');
+});
+// error middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(
-    `Server is up and running on port:${port}`
-  );
+  console.log(`Server is up and running on port:${port}`);
 });
