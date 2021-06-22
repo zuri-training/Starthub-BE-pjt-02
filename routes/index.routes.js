af0ec942956d8f528140954349a3c9adf327fe7c
izaakwalz <iblack.xyz@gmail.com>
2021-06-22 14:40:08 +0100

    Modefied

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
 router.use('/auth', require('./authRoute'));
-router.use('/projects', require('./projectRoutes'));
+// router.use('/projects', require('./projectRoutes')); // add async handler then un comment this line.
 
 module.exports = router;
